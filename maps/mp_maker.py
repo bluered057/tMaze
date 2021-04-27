f52af85b5eadb42527b5b6f270ebe793f8917b3f
bluered057 <bluered057mc@gmail.com>
2021-04-27 19:07:39 +0200

    added dev tools

--- maps/mp_maker.py
+++ maps/mp_maker.py
@@ -1,22 +1,27 @@
 import json
 
 map_basic = [
-	[0,1,1,1,1,1,1,1,0,0],
-	[0,1,0,0,0,0,0,1,0,0],
-	[0,1,1,1,0,1,0,1,0,0],
-	[0,0,0,0,0,1,0,0,0,0],
-	[0,1,1,1,1,1,1,1,0,0],
-	[0,1,0,0,0,0,1,0,0,0],
-	[0,1,0,1,1,0,1,0,1,0],
+	[0,0,0,0,0,0,1,0,0,0],
+	[0,1,1,1,1,0,1,0,1,1],
 	[0,1,0,0,1,0,1,0,1,0],
-	[0,1,1,1,1,0,1,0,1,0],
-	[0,0,0,0,0,0,1,0,1,0]
+	[0,1,0,1,1,0,1,0,1,0],
+	[0,1,0,0,0,0,1,0,1,0],
+	[0,1,1,1,1,0,0,0,0,0],
+	[0,1,0,2,1,0,1,1,1,1],
+	[0,1,0,1,1,0,1,0,0,0],
+	[0,1,0,0,1,0,1,1,1,0],
+	[1,1,1,0,0,0,0,0,0,0]
 ]
 
-map_spawn = (10,10)
-write_info = {'map_spawn':map_spawn, 'map_strc':map_basic}
+map_spawn = (504,144)
+map_color1 = (200,200,200)
+map_color2 = (150,150,150)
+map_color3 = (100,100,100)
+width = 10
+height = 10
+write_info = {'spawn':map_spawn, 'structure':map_basic, 'color1':map_color1, 'color2':map_color2, 'color3':map_color3, 'width':width, 'height':height}
 
-with open('0.mapp', 'w') as f:
+with open('1.mapp', 'w') as f:
 	json.dump(json.dumps(write_info), f)
 
 empty_map_basic = [
---
+++ reset.pyw
@@ -0,0 +1,7 @@
+import json
+
+with open('gamestate', 'w') as f:
+	json.dump(json.dumps('0'), f)
+
+pass
+
